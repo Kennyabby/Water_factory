37308c64bd1c303bb678eda6b3ed8b7aed28760d
sumendrapandey44@gmail.com <sumendrapandey44@gmail.com>
2023-10-06 11:01:55 +0100

    adding updates

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,8 +13,12 @@ const Home = ()=>{
                             <h2>OPEN THE DOOR TO BETTER LIVING</h2>
                             <h1 className='htitle'>Higher Quality Water For You and Your Family</h1>
                             <p className='hatt'>Professional installation and repair of various water-treatment 
-                            related systems, including water softener systems and water filtration systems.</p>
-
+                            related systems, including water softener systems and water filtration systems.
+                            </p>
+                            <div className='hbtncontent'>
+                                <div className='blbutton'>Free Water Test</div>
+                                <div className='wtbutton'>Our Services</div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -25,6 +29,11 @@ const Home = ()=>{
                 <div className='lm'>
                     <h1 className='lmtitle'> LIVE BETTER WITH </h1> 
                     <h1 className='lmtitle dbl'>HIGH QUALITY WATER</h1>
+                    <p className='lmhatt'>Our Mission at Jasmil water is to provide high-quality water to every customers. 
+                        We pride our selves on being responsive, honest, fairly-priced and customer-oriented. Jasmil water means water
+                        for you and your family with the added benefit of cost-savings and a positive enviromental impact
+                    </p>
+                    <div className='blbutton'>Contact Us</div>
                 </div>
                 <div className='rm'></div>
             </div>
